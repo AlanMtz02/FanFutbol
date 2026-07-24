9fd19261f72b02bd701f9f04d7bf30ed0f6ea4f1
Alan Martinez Lomas <alanmartinezlomas@gmail.com>
2026-07-24 14:44:44 -0600

    fix(backend): corregir tipo de dato de fecha_fin en esquema TorneoOutSchema

--- backend/schemas/torneo.py
+++ backend/schemas/torneo.py
@@ -18,7 +18,7 @@ class TorneoOutSchema(BaseModel):
     fase_actual:str
     equipo_campeon_id:Optional[int] = None #Valor por defecto
     creado_en:datetime
-    fecha_fin:Optional[str]=None #Valor por defecto
+    fecha_fin:Optional[datetime]=None #Valor por defecto
     admin_id:int
     
     class Config:
